ce37075fcb9a95e0b77797426ad679b7d225f608
bdangla <bdangla@github.com>
2017-06-01 15:40:35 -0600

    Primer Push

---
+++ test/src/Container.js
@@ -0,0 +1,16 @@
+import React , { Component } from 'react';
+
+class Container extends Component{
+	render() {
+		return (
+
+				<div>
+						<h1>Esto es una prueba!!!</h1>
+				</div>
+
+
+			);
+	}
+}
+
+export default Container;
