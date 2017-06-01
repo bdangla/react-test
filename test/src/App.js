25a4d3985e0b64124c77803f4c8b37dad980c72c
bdangla <bdangla@github.com>
2017-06-01 17:11:00 -0600

    conexion a BD

--- test/src/App.js
+++ test/src/App.js
@@ -2,16 +2,38 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Container from './Container.js';
+import * as firebase from 'firebase';
 
-
+// Initialize Firebase
+const config = {
+  apiKey: "AIzaSyA1F9rySRQ4yGuaAtHRP-BwQPBPvgultrM",
+  authDomain: "test-react-c888a.firebaseapp.com",
+  databaseURL: "https://test-react-c888a.firebaseio.com",
+  projectId: "test-react-c888a",
+  storageBucket: "test-react-c888a.appspot.com",
+  messagingSenderId: "273132129204"
+};
+firebase.initializeApp(config);
 class App extends Component {
-  render() {
+  constructor(){
+    super()
+    this.state={name:'javier'}
+  }
+  componentWillMount(){
+    const nameRef = firebase.database().ref().child('name');
+      nameRef.on('value', snapshot => {  
+        this.setState({
+          name: snapshot.val()
+        });
+      });
+    }
+  render(){
     return (
       <div className="App">
         < Container />
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>{this.state.name}</h2>
         </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
